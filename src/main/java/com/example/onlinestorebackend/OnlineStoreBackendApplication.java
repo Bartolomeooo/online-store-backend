9ec2249aec2cbb4530d4f46fafb9436b58d14bf8
Bartek <bartoszkawiak@gmail.com>
2024-12-30 20:50:28 +0100

    Enhance createReview method

--- src/main/java/com/example/onlinestorebackend/OnlineStoreBackendApplication.java
+++ src/main/java/com/example/onlinestorebackend/OnlineStoreBackendApplication.java
@@ -1,5 +1,6 @@
 package com.example.onlinestorebackend;
 
+import com.example.onlinestorebackend.service.UserService;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,6 +10,7 @@ public class OnlineStoreBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(OnlineStoreBackendApplication.class, args);
+
     }
 
 }
--- src/main/java/com/example/onlinestorebackend/controller/ReviewController.java
+++ src/main/java/com/example/onlinestorebackend/controller/ReviewController.java
@@ -2,17 +2,25 @@ package com.example.onlinestorebackend.controller;
 
 import com.example.onlinestorebackend.DTO.ReviewRequestDTO;
 import com.example.onlinestorebackend.entity.Review;
+import com.example.onlinestorebackend.entity.User;
 import com.example.onlinestorebackend.service.ReviewService;
+import com.example.onlinestorebackend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
     @Autowired
     private ReviewService reviewService;
+
+    @Autowired
+    private UserService userService;
 
     @GetMapping
     public List<Review> getAllReviews() {
@@ -30,8 +38,19 @@ public class ReviewController {
     }
 
     @PostMapping
-    public Review createReview(@RequestBody ReviewRequestDTO reviewRequest) {
-        return reviewService.createReview(reviewRequest);
+    public ResponseEntity<?> createReview(@RequestBody ReviewRequestDTO reviewRequest) {
+        String username = SecurityContextHolder.getContext().getAuthentication().getName();
+
+        User user = userService.findByUsername(username)
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        reviewRequest.setUserId(user.getId());
+        try {
+            Review review = reviewService.createReview(reviewRequest);
+            return ResponseEntity.ok(review);
+        } catch (RuntimeException e) {
+            return ResponseEntity.badRequest().body(Map.of("error", e.getMessage()));
+        }
     }
 
     @DeleteMapping("/{id}")
